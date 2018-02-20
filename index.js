6b93107036f5dca5a9bb379f292e59bbda9be832
Karolee <kcarlson957@gmail.com>
2018-02-19 17:49:47 -0800

    horizontal and vertical logic work

--- index.js
+++ index.js
@@ -7,6 +7,45 @@ board.forEach(element => {
 
 }
 
+const CheckWinner = (board, turn) => {
+  let winner, check;
+  if(turn){
+    check = 'x'
+  } else {
+    check = 'o'
+  }
+  //horizontal
+  board.forEach(row => {
+    let count = 0;
+    row.forEach(index => {
+      if(index === check) {
+        count += 1
+      }
+      if(count === 3){
+        winner = check;
+      }
+    })
+  })
+  //vertical
+  for(let col = 0; col < 3; col++ ) {
+    let count = 0;
+    for(let row = 0; row < 3; row++) { 
+      if(board[row][col] === check) {
+        count += 1
+      }
+    } 
+    if(count === 3) {
+      winner = check;
+    } 
+  }
+//diagnal
+
+if(winner){
+  console.log(winner, 'won!' )
+} else { GetUserResponse(board, !turn) }
+
+}
+
 const GetUserResponse = (board, turn) => {
   prompt.get(['row', 'column'], (err, result) => {
     console.log(' You chose,', result.row, result.column);
@@ -15,8 +54,9 @@ const GetUserResponse = (board, turn) => {
     } else {
       board[result.row][result.column] = 'o'
     }
-    turn = !turn;
+    
     printBoard(board);
+    CheckWinner(board, turn);
   })
 }
 
@@ -24,8 +64,9 @@ prompt.start();
 console.log("TICTACTOE")
 
 const Game = () => {
-  //true indicates player 1
+  //true indicates player 1 'x'
  let turn = true;
+ let round = 0;
   let board = [[0,0,0],[0,0,0],[0,0,0]];
   printBoard(board);
   GetUserResponse(board, turn)
